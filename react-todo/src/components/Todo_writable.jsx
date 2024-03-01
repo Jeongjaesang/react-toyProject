8fc2733fa85bfdf318015708b45408a0dac06dba
jaesang <wotkd09093@gmail.com>
2024-03-01 18:34:25 +0900

    엔터 눌러 추가하기

--- react-todo/src/components/Todo_writable.jsx
+++ react-todo/src/components/Todo_writable.jsx
@@ -1,7 +1,7 @@
 import Button from "./Button";
 import PropTypes from "prop-types";
 import Category from "./Category";
-import { useContext, useRef, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { TodoDispatchContext } from "../App";
 import { copyTodo_daily } from "../services/services";
 
@@ -17,6 +17,8 @@ const Todo_writable = ({ curTodo_daily }) => {
   const todoItemId = useRef(10); // Todo_item의 아이디가 모두 달라야함
   // 어제의 Todo_item을 오늘의 Todo_Item으로 옮길 수도 있는 것을 고려하면..
 
+  const inputRef = useRef(null);
+
   const { onDeleteTodoDaily, onUpdateTodoDaily } =
     useContext(TodoDispatchContext);
 
@@ -27,6 +29,12 @@ const Todo_writable = ({ curTodo_daily }) => {
   };
 
   const handleCreateTodoItem = () => {
+    if (!inputValue) {
+      alert("할 일을 입력하셔야 추가됩니다.");
+      inputRef.current.focus();
+      return;
+    }
+
     const newTodo_daily = copyTodo_daily(curTodo_daily);
     newTodo_daily.not_started.push({
       id: todoItemId.current,
@@ -37,6 +45,22 @@ const Todo_writable = ({ curTodo_daily }) => {
     setInputValue("");
   };
 
+  useEffect(() => {
+    const handleKeydown = (e) => {
+      if (e.key == "Enter") {
+        handleCreateTodoItem();
+        alert("enter pressed");
+      }
+    };
+
+    // 입력하고 엔터 누를 때 추가되게 하기
+    document.addEventListener("keydown", handleKeydown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeydown);
+    };
+  }, [inputValue]); // 노란줄, 뭐가 문제일까? 일단 보류..
+
   return (
     <div className="view Todo_writable">
       <section className="add_TodoItem_area">
@@ -46,6 +70,7 @@ const Todo_writable = ({ curTodo_daily }) => {
             placeholder="할 일을 입력해 주세요"
             value={inputValue}
             onChange={onChangeInputValue}
+            ref={inputRef}
           />
         </div>
         <Button type="create" text="create" onClick={handleCreateTodoItem} />
