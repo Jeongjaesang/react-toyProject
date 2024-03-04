036c7460a21d2bf5622f11af8b4da9a8d15c7a33
jaesang <wotkd09093@gmail.com>
2024-03-04 17:59:30 +0900

    미완료된 todo 갯수 표시하기

--- react-todo/src/components/TodoRoot.jsx
+++ react-todo/src/components/TodoRoot.jsx
@@ -7,8 +7,11 @@ import Empty from "./Empty";
 const TodoRoot = () => {
   const [curDate, setCurDate] = useState(new Date());
   const [curTodo_daily, setcurTodo_daily] = useState(null);
+  const [notCompletedTodoes, setNotCompletedTodoes] = useState(0);
 
   const state = useContext(TodoStateContext);
+
+  const todoMessage = `*총 ${notCompletedTodoes}개의 미완료된 todo가 존재합니다.`;
 
   const headText = `${curDate.getFullYear()}년  ${
     curDate.getMonth() + 1
@@ -31,6 +34,17 @@ const TodoRoot = () => {
   };
 
   useEffect(() => {
+    let cnt = 0;
+
+    state.forEach((todo_daily) => {
+      cnt += todo_daily.not_started.length;
+      cnt += todo_daily.in_progress.length;
+    });
+
+    setNotCompletedTodoes(cnt);
+  }, [state, curDate]);
+
+  useEffect(() => {
     // state에서 현재 날짜에 해당하는 Todo가 있으면 가져온다.
     const targetTodoDaily = state.find(
       (todoDaily) => todoDaily.id === curDate.toDateString()
@@ -45,6 +59,7 @@ const TodoRoot = () => {
         {headText}
         <Button type="default" text=">" onClick={increaseDate} />
       </header>
+      <h3 className="todoMessage">{todoMessage}</h3>
       {curTodo_daily ? (
         <Todo_writable curTodo_daily={curTodo_daily} />
       ) : (
